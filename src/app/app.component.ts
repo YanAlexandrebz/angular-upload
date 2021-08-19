3c4fb23dd31324e89b471f86c63c3992226e5dbf
Yan Alexandre <rm77740@fiap.com.br>
2021-08-19 15:59:23 -0300

    Adicionado arquivos iniciais

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { UploadService } from './services/upload.service';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'upload-arquivos';
+
+  arquivos: File[] = [];//array para guardar os arquivos
+  formInvalido = false;//flag para controlar a exibicao do alert
+  erroUpload = false;//flag para controlar a exibicao do alert de envio do arquivo
+  urlsUpload: string[] = [];//array para armazenar as urls de retorno
+
+  constructor(private UploadService: UploadService){}
+
+  armazenarArquivo($event: any){
+    this.arquivos = $event.target.files;
+    this.formInvalido = false;
+  }
+  
+  enviarArquivo($event: any){
+    //preventDefault() - evita de ficar recarregando a pag a cada clique
+    $event.preventDefault();
+    //valida se tem algum arquivo para ser enviado
+    if (this.arquivos.length == 0){
+      this.formInvalido = true;
+      return;
+    }
+    this.erroUpload = false;
+    this.UploadService.upload(this.arquivos).subscribe(
+      dados => this.urlsUpload = dados as string[], //sucesso
+      () => this.erroUpload = true //erro
+    );
+  
+  }
+
+ 
+
 }
